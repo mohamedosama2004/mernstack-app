87affb6a2f40047e8097004a292f5d4ba64205ba
Mohamed Osama <146884487+mohamedosama2004@users.noreply.github.com>
2026-07-23 07:55:58 +0300

    production modification

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,24 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "../config/db.js";
 import dotenv from "dotenv";
+import path from "path";
 import rateLimiter from "../middleware/ratelimiter.js";
 dotenv.config();
 const port = process.env.PORT || 5001;
 const app = express();
+const __dirname = path.resolve();
 // middle ware
 app.use(express.json()); // this  middle layer will parse the json bodies
 
 // our middle ware
 app.use(rateLimiter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 app.use("/api/notes", notesRoutes);
 connectDB().then(() => {
   app.listen(port, () => {
